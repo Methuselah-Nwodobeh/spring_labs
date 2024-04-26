813450ce5f0955960b2ec29a74c7e265ee3dd252
Methuselah-Nwodobeh <methuselah.nwodobeh@amalitech.com>
2024-04-26 12:36:16 +0000

    Initial commit

---
+++ src/main/java/com/amalitech/upskilling/spring_labs/SpringLabsApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.upskilling.spring_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabsApplication.class, args);
+	}
+
+}
